1fe2746d6b26e7abe470366c940de8bf5d757083
saad909 <69252484+saad909@users.noreply.github.com>
2020-10-31 18:22:10 +0500

    Add files via upload

---
+++ scrt.sh
@@ -0,0 +1,6 @@
+sudo tar -xvzf ./scrt-sfx-8.7.3.2279.ubuntu19-64.tar.gz -C /usr/share
+#wget http://download.boll.me/securecrt_linux_crack.pl
+sudo mv /usr/share/scrt-8.7.3 /usr/share/SecureCRT
+sudo perl securecrt_linux_crack.pl /usr/share/SecureCRT/SecureCRT
+#Now, Enter the Information given in terminal from Above Command
+#settings.xml -> settings imported from secureCrt
